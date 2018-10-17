48c9f6d8bcf9a2deb3a3aa82a859f600c2ee1dba
MINDOCK <parksy8189@gmail.com>
2018-10-17 15:27:33 +0900

    refactor: Point 생성자 수정
    1. id를 따로 인자로 받지 않고 생성자 안에서 UUID 생성하도록 구현

--- src/main/java/com/triple/pointservice/domain/Point.java
+++ src/main/java/com/triple/pointservice/domain/Point.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
@@ -22,6 +23,7 @@ public class Point {
     private String action;
 
     public Point(User user, String content, int point, String pointType, String relatedId, String action) {
+        this.id = UUID.randomUUID().toString();
         this.user = user;
         this.content = content;
         this.point = point;
